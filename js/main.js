39000f3459c01987bf60fb30f29ea3b497b9bdc7
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-07-25 16:39:24 +0900

    반응형 이미지 추가, 토글 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 // main.js
 $(function(){
 	
-	
+	// 모바일 토글 메뉴
+	// #toggle click하면 nav가 toggle됨(toggleClass)
+	$("#toggle").click(function(){
+		$("header nav").toggleClass("on");
+	});
 
 }); //$
